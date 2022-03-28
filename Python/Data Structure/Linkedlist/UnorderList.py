e4073c19b27f93828adb0b2692c47b445b758112
shicw <stushi0107@gmail.com>
2022-03-28 22:36:49 +0800

    init

---
+++ Python/Data Structure/Linkedlist/UnorderList.py
@@ -0,0 +1,18 @@
+from Node import Node
+
+class UnorderList():
+    def __init__(self) -> None:
+        self.head = None
+    def isempty(self):
+        return self.head == None
+    def add(self,item):
+        temp = Node(item)
+        temp.setnext(self.head)
+        self.head = temp
+
+if __name__ == '__main__':
+    mylist = UnorderList()
+    mylist.add(10)
+    print('creat')
+
+        
