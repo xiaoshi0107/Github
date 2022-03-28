5da4642c2dedfedcd78a35a882b3e80bb98a15ed
shicw <stushi0107@gmail.com>
2022-03-28 16:06:35 +0800

    init

--- Python/Data Structure/Linkedlist/Node.py
+++ Python/Data Structure/Linkedlist/Node.py
@@ -1,3 +1,16 @@
 class Node():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, initdata) -> None:
+        self.data = initdata
+        self.next = None
+
+    def getdata(self,):
+        return self.data
+
+    def getnext(self,):
+        return self.next
+
+    def setdata(self, newdata):
+        self.data = newdata
+
+    def setnext(self, newnext):
+        self.next = newnext
