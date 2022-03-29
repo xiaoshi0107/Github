ebef4c2300c1efc0cdc46a456750b614d99cc695
shicw <stushi0107@gmail.com>
2022-03-29 15:49:06 +0800

    init

--- Python/Data Structure/Linkedlist/UnorderList.py
+++ Python/Data Structure/Linkedlist/UnorderList.py
@@ -1,5 +1,6 @@
 from Node import Node
 
+
 class UnorderList():
     def __init__(self) -> None:
         self.head = None
@@ -7,11 +8,33 @@ class UnorderList():
     def isempty(self):
         return self.head == None
 
-    def add(self,item):
+    def add(self, item):
         temp = Node(item)
         temp.setnext(self.head)
         self.head = temp
 
+    def length(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.getnext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None and not found:
+            if current.data == item:
+                found = True
+            else:
+                current = current.getnext()
+        
+        return found
+
+
 if __name__ == '__main__':
+    print('start')
     mylist = UnorderList()
     mylist.add(10)
+    print(mylist.search(10))
